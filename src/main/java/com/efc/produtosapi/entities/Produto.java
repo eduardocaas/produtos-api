d3d950e9716376414261e24f85197f1a7fb981cf
Eduardo Castro <eduardofc.dev@gmail.com>
2023-01-25 19:08:44 -0300

    Propriedades de Produto

--- src/main/java/com/efc/produtosapi/entities/Produto.java
+++ src/main/java/com/efc/produtosapi/entities/Produto.java
@@ -1,4 +1,48 @@
 package com.efc.produtosapi.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nome;
+    private float preco;
+
+    public Produto() {
+    }
+
+    public Produto(String nome, float preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
 }

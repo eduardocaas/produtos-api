a019e5650fd00caed5f5e3971e331778f46c9562
Eduardo Castro <eduardofc.dev@gmail.com>
2023-01-25 19:09:20 -0300

    Serviço para Produto

--- src/main/java/com/efc/produtosapi/services/ProdutoService.java
+++ src/main/java/com/efc/produtosapi/services/ProdutoService.java
@@ -1,4 +1,47 @@
 package com.efc.produtosapi.services;
 
+import com.efc.produtosapi.entities.Produto;
+import com.efc.produtosapi.repositories.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
 public class ProdutoService {
+
+    @Autowired
+    ProdutoRepository produtoRepository;
+
+    public List<Produto> findAll(){
+        return produtoRepository.findAll();
+    }
+
+    public Optional<Produto> findById(int id){
+        return produtoRepository.findById(id);
+    }
+
+    public Optional<Produto> findByNome(String nome){
+        return produtoRepository.findByNome(nome);
+    }
+
+    public void save(Produto produto){
+        produtoRepository.save(produto);
+    }
+
+    public void delete(int id){
+        produtoRepository.deleteById(id);
+    }
+
+    public boolean existsById(int id){
+        return produtoRepository.existsById(id);
+    }
+
+    public boolean existsByNome(String nome){
+        return produtoRepository.existsByNome(nome);
+    }
+
 }

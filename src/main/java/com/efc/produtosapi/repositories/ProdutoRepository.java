71bb39816f9c55ed0a1d84e6e7a56c97a266f93a
Eduardo Castro <eduardofc.dev@gmail.com>
2023-01-25 19:08:56 -0300

    Repository para Produto

--- src/main/java/com/efc/produtosapi/repositories/ProdutoRepository.java
+++ src/main/java/com/efc/produtosapi/repositories/ProdutoRepository.java
@@ -1,4 +1,13 @@
 package com.efc.produtosapi.repositories;
 
-public class ProdutoRepository {
+import com.efc.produtosapi.entities.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+    Optional<Produto> findByNome(String nome);
+    boolean existsByNome(String nome);
 }

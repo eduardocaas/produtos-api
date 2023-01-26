fdc963856acd50ba79747e9d03bd70f115627ba9
Eduardo Castro <eduardofc.dev@gmail.com>
2023-01-25 23:00:54 -0300

    Produto DTO criado

--- src/main/java/com/efc/produtosapi/dtos/ProdutoDTO.java
+++ src/main/java/com/efc/produtosapi/dtos/ProdutoDTO.java
@@ -1,4 +1,36 @@
 package com.efc.produtosapi.dtos;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
 public class ProdutoDTO {
+
+    @NotBlank
+    private String nome;
+    @Min(0)
+    private Float preco;
+
+    public ProdutoDTO() {
+    }
+
+    public ProdutoDTO(@NotBlank String nome, @Min(0) Float preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
 }

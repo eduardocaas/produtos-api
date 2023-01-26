06f16880a558b31602f3cca57ff4772e7d789a57
Eduardo Castro <eduardofc.dev@gmail.com>
2023-01-25 23:00:16 -0300

    Update Mensagem.java

--- src/main/java/com/efc/produtosapi/dtos/Mensagem.java
+++ src/main/java/com/efc/produtosapi/dtos/Mensagem.java
@@ -1,4 +1,18 @@
 package com.efc.produtosapi.dtos;
 
 public class Mensagem {
+
+    private String mensagem;
+
+    public Mensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
 }
